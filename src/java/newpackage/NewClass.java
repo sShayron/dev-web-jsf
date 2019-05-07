eebeb31d8a59beaf622d920d1bcce74294d8bc9c
Shayron Aguiar <shayron.aguiar@gmail.com>
2019-05-06 22:21:19 -0300

    Update NewClass.java

--- src/java/newpackage/NewClass.java
+++ src/java/newpackage/NewClass.java
@@ -1,21 +1,23 @@
 package newpackage;
 
+import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.RequestScoped;
+import javax.faces.context.FacesContext;
 
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 /**
  *
  * @author alunodev06
  */
-@ManagedBean(name="usuarioBean")
+@ManagedBean(name = "usuarioBean")
 @RequestScoped
 public class NewClass {
+
     public String name;
     public String senha;
 
@@ -29,9 +31,21 @@ public class NewClass {
 
     public String getSenha() {
         return senha;
-    }   
+    }
 
     public void setSenha(String senha) {
         this.senha = senha;
+    }
+
+    public String valida() {
+        if (this.senha.equals("123") && this.name.equals("shayro")) {            
+            return "case1";
+        }
+        FacesContext.getCurrentInstance().addMessage(
+                null,
+                new FacesMessage("SEVERITY_ERROR: Senha incorreta...", "É necessário digita a senha corretamente")
+        );
+
+        return null;
     }
 }
